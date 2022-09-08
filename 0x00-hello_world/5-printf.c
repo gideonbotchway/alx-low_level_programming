a4d4647522610d7e3ebc05a7dc6d76c0363c1834
gideonbotchway <botchwaygideon5@gmail.com>
2022-09-08 14:57:09 -0700

    task 4 to 101

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - outputs or displays the proper grammer,
+ * but the outcome is a piece of art,
+ *
+ * Return: 0 if exited properly,  otherwise display a non-zer number
+ */
+int main(void)
+{
+	 printf("with proper grammar, but the outcome is a piece of art,\n");
+	 return (0);
+}
+
