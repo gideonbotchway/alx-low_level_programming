9e8a0e7c131b1c89036d216461bbbccebc4b098e
gideonbotchway <botchwaygideon5@gmail.com>
2022-09-27 00:45:10 -0700

    tasks

--- 0x07-pointers_arrays_strings/Main.h
+++ 0x07-pointers_arrays_strings/Main.h
@@ -1,7 +1,7 @@
-ifndef FILE_HOLBERTON
+#ifndef FILE_HOLBERTON
 #define FILE_HOLBERTON
 
-#int _putchar(char c);
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
