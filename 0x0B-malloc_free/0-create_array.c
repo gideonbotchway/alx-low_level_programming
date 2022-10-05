0c245f54cfd8fbd30c64f6ac0abf863c0c52ce8d
gideonbotchway <botchwaygideon5@gmail.com>
2022-10-05 03:12:48 -0700

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * create_array - creates an array of chars,
+ * *create_array - creates an array of chars,
  * and initializes it with a specific char
  * @size: size of the array to create
  * @c: char to initialize the array c
@@ -29,5 +29,6 @@ i++;
 }
 
 *(p + i) = '\0';
+
 return (p);
 }
